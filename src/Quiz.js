2726d4fea9a194531a019a25096a99b871b29171
Dovie Shalev <dovieshalev@Dovies-MacBook-Pro.local>
2021-01-20 12:34:57 +0200

    question and Explanation JSX objects working, need to add ending

--- src/Quiz.js
+++ src/Quiz.js
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom";
 
 function Quiz() {
   const [quizJson, setQuizJson] = useState({});
-  const [currentQuestion, setCurrentQuestion] = useState({});
+  const [currentQuestion, setCurrentQuestion] = useState({ answers: [] });
+  const [showQuestion, setShowQuestion] = useState(true);
 
   // TODO load in json based on url using match
   useEffect(() => {
@@ -15,7 +16,7 @@ function Quiz() {
 
   useEffect(() => {
     console.log("Running useEffect on quizJson update");
-    if (quizJson !== null) {
+    if (quizJson.questions !== undefined) {
       setCurrentQuestion(quizJson.questions[0]);
       console.log("currentQuestion is:");
       console.log(currentQuestion);
@@ -28,7 +29,15 @@ function Quiz() {
         <h3 className="questionText">{currentQuestion.questionText}</h3>
         <div className="answerArray">
           {currentQuestion.answers.map((answer) => {
-            return <button>{answer.answerText}</button>;
+            return (
+              <button
+                className="answerButton"
+                onClick={answerButtonPressed}
+                value={answer.correct}
+              >
+                {answer.answerText}
+              </button>
+            );
           })}
         </div>
       </div>
@@ -36,9 +45,44 @@ function Quiz() {
   };
 
   var Explanation = () => {
-    return <h1>Explanation!</h1>;
+    return (
+      <div className="explanationWrapper">
+        <h3 className="explanationText">{currentQuestion.explanationText}</h3>
+        <button
+          className="buttonText"
+          id="explanationButton"
+          onClick={explanationButtonPressed}
+        >
+          Next Question
+        </button>
+      </div>
+    );
   };
 
+  function answerButtonPressed(e) {
+    console.log("Answer button pressed, value is: ");
+    console.log(e.target.value);
+    if (e.target.value === "true") {
+      console.log("Found the right answer");
+      setShowQuestion(false);
+    } else {
+      e.target.style.color = "#eb4460";
+    }
+  }
+
+  function explanationButtonPressed(e) {
+    var nextQuestion;
+    for (var i = 0; i < quizJson.questions.length; i++) {
+      const question = quizJson.questions[i];
+      if (question === currentQuestion) {
+        console.log("found same question!");
+        nextQuestion = i + 1;
+        break;
+      }
+    }
+    setCurrentQuestion(quizJson.questions[nextQuestion]);
+    setShowQuestion(true);
+  }
   return (
     <div className="main">
       <div className="header">
@@ -52,7 +96,7 @@ function Quiz() {
         </h2>
       </div>
       <div className="quizPage">
-        {<Question /> ? <Question /> : <Explanation />}
+        {showQuestion ? <Question /> : <Explanation />}
       </div>
     </div>
   );
